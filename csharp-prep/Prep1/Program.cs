ea2a1155d4728569a23b2a773cb2f914a98c4427
kenslin-pro <kenslinerohine@gmail.com>
2023-01-27 00:13:12 +0300

    Added change

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep1 World!");
+    Console.Write("What is your first name ?");
+    string first = Console.ReadLine();
+    Console.Write("What is your second name?");
+    string last = Console.ReadLine();
+    Console.WriteLine($"Your name is {last}, {first} {last}.");
+
+  }
 }
