7e3be8e412a461df5b6f4b5c91f3e19dfd0d4c94
kenslin-pro <kenslinerohine@gmail.com>
2023-01-29 01:18:14 +0300

    added change

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -19,19 +19,24 @@ class Program
   }
 {
     static void Main(string[] args)
+
+  //first job//
   {
     Job job1 = new Job();
-    job1._jobTitle = "Software Engineer";
-    job1._company = "Microsoft";
+    job1._jobTitle = "Cashier";
+    job1._company = "Aerial";
     job1._startYear = 2019;
     job1._endYear = 2022;
+
+    //second job//
+
     Job job2 = new Job();
-    job2._jobTitle = "Manager";
-    job2._company = "Apple";
+    job2._jobTitle = "Secretary";
+    job2._company = "Detol";
     job2._startYear = 2022;
     job2._endYear = 2023;
     Resume myResume = new Resume();
-    myResume._name = "Allison Rose";
+    myResume._name = "Kenslin Rohin";
     myResume._jobs.Add(job1);
     myResume._jobs.Add(job2);
 
