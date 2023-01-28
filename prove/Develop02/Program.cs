23fb27d8ba33f7b0cd8a317de43793e9208ad58e
kenslin-pro <kenslinerohine@gmail.com>
2023-01-29 00:52:05 +0300

    added change

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Develop02 World!");
+  }
+  public class Job
+  {
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+    public void Display()
     {
-        Console.WriteLine("Hello Develop02 World!");
+      Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
+  }
+{
+    static void Main(string[] args)
+  {
+    Job job1 = new Job();
+    job1._jobTitle = "Software Engineer";
+    job1._company = "Microsoft";
+    job1._startYear = 2019;
+    job1._endYear = 2022;
+    Job job2 = new Job();
+    job2._jobTitle = "Manager";
+    job2._company = "Apple";
+    job2._startYear = 2022;
+    job2._endYear = 2023;
+    Resume myResume = new Resume();
+    myResume._name = "Allison Rose";
+    myResume._jobs.Add(job1);
+    myResume._jobs.Add(job2);
+
+    myResume.DisplayResumeDetails();
+    myResume.Display();
+  }
+}
 }
