f94f3944c5981377e3bd8b58c48bcb0f01d959b3
kenslin-pro <kenslinerohine@gmail.com>
2023-02-04 15:15:51 +0300

    Added change

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,8 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep2 World!!!!!!");
+  }
 }
