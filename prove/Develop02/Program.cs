4750b4ecbd888beb77c0241a0073903177159c87
kenslin-pro <kenslinerohine@gmail.com>
2023-02-07 01:52:56 +0300

    added change

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,47 +1,44 @@
 using System;
+public class Journal
 
 class Program
 {
   static void Main(string[] args)
   {
     Console.WriteLine("Hello Develop02 World!");
-  }
-  public class Job
+
+        public string _filename = "";
+  public List<Entry> _entries = new List<Entry>();
+  public void Display()
   {
-    public string _jobTitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
-    public void Display()
+    foreach (Entry entry in _entries)
     {
-      Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+      entry.Format();
     }
   }
-{
-    static void Main(string[] args)
-
-  //first job//
+  public void SaveFile()
   {
-    Job job1 = new Job();
-    job1._jobTitle = "Cashier";
-    job1._company = "Aerial";
-    job1._startYear = 2019;
-    job1._endYear = 2022;
-
-    //second job//
-
-    Job job2 = new Job();
-    job2._jobTitle = "Secretary";
-    job2._company = "Detol";
-    job2._startYear = 2022;
-    job2._endYear = 2023;
-    Resume myResume = new Resume();
-    myResume._name = "Kenslin Rohin";
-    myResume._jobs.Add(job1);
-    myResume._jobs.Add(job2);
-
-    myResume.DisplayResumeDetails();
-    myResume.Display();
+    using (StreamWriter outputFile = new StreamWriter(_filename))
+    {
+      foreach (Entry entry in _entries)
+      {
+        outputFile.WriteLine($"{entry._date}  , {entry._prompt} , {entry._response}");
+      }
+    }
   }
-}
+  public void LoadFile()
+  {
+    string[] lines = System.IO.File.ReadAllLines(_filename);
+    foreach (string line in lines)
+    {
+      string[] parts = line.Split(",");
+      Entry oldEntry = new Entry();
+      pastEntry._date = parts[0];
+      pastEntry._prompt = parts[1];
+      pastEntry._response = parts[2];
+      _entries.Add(pastEntry);
+    }
+
+  }
+
 }
