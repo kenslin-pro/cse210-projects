789b7e7db5cf7372d770fcb3814a1c8d49eda3b5
kenslin-pro <kenslinerohine@gmail.com>
2023-02-07 13:43:47 +0300

    added change

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,13 @@ class Program
   static void Main(string[] args)
   {
     Console.WriteLine("Hello Prep1 World!");
-    Console.Write("What is your first name ?");
-    string first = Console.ReadLine();
-    Console.Write("What is your second name?");
-    string last = Console.ReadLine();
-    Console.WriteLine($"Your name is {last}, {first} {last}.");
+    Console.Write("What is your first name? ");
+    string name1 = Console.ReadLine();
+
+    Console.Write("What is your second name? ");
+    string name2 = Console.ReadLine();
+
+    Console.WriteLine($"your first name is{name1} and second name is{name2}");
 
   }
 }
