ec63059d2209f13c4d9d859cca3f56eabe913e2d
kenslin-pro <kenslinerohine@gmail.com>
2023-02-27 23:46:31 +0300

    added changes

--- prove/Develop04/reflection.cs
+++ prove/Develop04/reflection.cs
@@ -1,34 +1,29 @@
 public class ReflectionActivity : Activity
 {
-  // The list of prompts
+
   public List<string> Prompts { get; set; }
 
-  // The list of questions
+
   public List<string> Questions { get; set; }
 
-  // Override the RunActivity method from the base class
   public override void RunActivity()
   {
-    // Show the starting message
+
     Console.WriteLine($"Starting {Name} which will last for {Duration} seconds.");
     Console.WriteLine($"Description: {Description}");
     Console.WriteLine("Prepare to begin...");
-
-    // Pause for a few seconds
     System.Threading.Thread.Sleep(3000);
 
-    // Get a random prompt
     Random random = new Random();
     int promptIndex = random.Next(0, Prompts.Count - 1);
     Console.WriteLine(Prompts[promptIndex]);
 
-    // Ask the questions
     for (int i = 0; i < Questions.Count; i++)
     {
       Console.WriteLine(Questions[i]);
       System.Threading.Thread.Sleep(Duration * 1000);
 
-      // Show a spinner while the program is paused
+      // the spinner
       Console.CursorLeft = 0;
       Console.Write("|");
       System.Threading.Thread.Sleep(1000);
@@ -43,7 +38,7 @@ public class ReflectionActivity : Activity
       System.Threading.Thread.Sleep(1000);
     }
 
-    // Show the ending message
+    // the end
     Console.WriteLine("Good job!");
     Console.WriteLine($"You have completed the {Name} activity for {Duration} seconds.");
     Console.WriteLine("Thank you for participating.");
